fb1851e3d8d33a610534477e12b5bb626c82be73
William-hu0901 <delong201314@gmail.com>
2025-11-05 18:49:18 +0800

    change chinese to english

--- src/main/java/org/daodao/restserver/AppHealthIndicator.java
+++ src/main/java/org/daodao/restserver/AppHealthIndicator.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Component;
 public class AppHealthIndicator implements HealthIndicator {
     @Override
     public Health health() {
-        // 你可以在这里添加逻辑来检查你的应用是否健康，例如检查数据库连接等。
-        int errorCode = check(); // 示例方法，你需要实现它来检查健康状态。
+        int errorCode = check(); // check health of app。
         if (errorCode != 0) {
             return Health.down().withDetail("Error Code", errorCode).build();
         }
@@ -17,8 +16,8 @@ public class AppHealthIndicator implements HealthIndicator {
     }
 
     private int check() {
-        // 实现你的检查逻辑，例如数据库连接测试等。
-        return 0; // 返回0表示无错误。
+        // can implement check logic here.
+        return 0; // return 0 if health, non-zero otherwise.
     }
 }
 
