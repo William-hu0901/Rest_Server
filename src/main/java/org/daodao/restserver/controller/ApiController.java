ca575dc10fbfdc0a6811486f14617a848dc99f02
William-hu0901 <delong201314@gmail.com>
2025-11-13 15:50:55 +0800

    add new POST endpoint querydata

--- src/main/java/org/daodao/restserver/controller/ApiController.java
+++ src/main/java/org/daodao/restserver/controller/ApiController.java
@@ -1,8 +1,8 @@
 package org.daodao.restserver.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.daodao.restserver.dto.QueryRequest;
+import org.daodao.restserver.dto.QueryResponse;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -10,5 +10,14 @@ public class ApiController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
+    }
+
+    @PostMapping("/queryData")
+    public QueryResponse queryData(@RequestBody QueryRequest request) {
+        // 模拟数据库查询结果
+        String result = "Query executed successfully for user: " + request.getUsername() + 
+                       " with SQL: " + request.getSql();
+        
+        return new QueryResponse("success", result, null);
     }
 }
